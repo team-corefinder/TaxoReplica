811f6090be04980aec78ce402392261b6a7101f3
김재성 (Kim, Jaesung) <15705385+sundoufu@users.noreply.github.com>
2021-11-16 17:25:42 +0000

    word2vec 추가, conda 설정

--- text-classifier/train.py
+++ text-classifier/train.py
@@ -4,64 +4,94 @@ import torch
 import gensim
 import dgl
 import dgl.function as fn
+import torch.optim as optim
 from torch import nn
 from encoder import DocuEncoder, ClassEncoder
 from layer import GCN
 from classifier import TextClassifier
 from preprocessor import TaxoDataManager, DocumentManager
 from gensim.test.utils import datapath
+from gensim.models import word2vec
 
 
 
 if __name__ == '__main__':
-    print(torch.cuda.get_device_name(0))
-    print(torch.version)
-    print(torch.cuda.is_available())
-    d_encoder = DocuEncoder()
-
-    document = """
-            An atom is the smallest unit of ordinary matter that forms a chemical element. 
-            Every solid, liquid, gas, and plasma is composed of neutral or ionized atoms. 
-            Atoms are extremely small, typically around 100 picometers across. 
-            They are so small that accurately predicting their behavior using classical 
-            physics—as if they were tennis balls, for example—is not possible due to quantum effects.
-            """
-    tokens = d_encoder.Tokenize(document)
+        print(torch.cuda.is_available())
 
 
-    #document_representation = d_encoder.Encode(tokens)
+        root = '/root/text-classifier/TaxoReplica/text-classifier/'
+        train_file = 'train-with-core-class-1000.jsonl'
+        taxonomy_file = 'taxonomy.json'
+        data_name = 'amazon'
+
+        d_encoder = DocuEncoder()
+        #document_representation = d_encoder.Encode(tokens)
+
+        word2vec_model = word2vec.Word2Vec.load(root + 'pretrained/' + 'embedding')
+        W = word2vec_model.wv.vector_size
+
+        gcn_model = GCN(W, W, W, 2, nn.ReLU())
+
+        class_encoder = ClassEncoder(gcn_model, word2vec_model)
+
+        text_classifier = TextClassifier(class_encoder, (W, 768))
+
+        g = dgl.DGLGraph()
 
 
-    word2vec_model = gensim.models.KeyedVectors.load_word2vec_format(datapath('word2vec_pre_kv_c'))
+        data_manager = TaxoDataManager(root + 'processed_data/', taxonomy_file, data_name, word2vec_model)
+        data_manager.load_all()
+        dm = DocumentManager(train_file, root + 'processed_data/', 'amazon_train', d_encoder.Tokenize, d_encoder.Encode,  data_manager)
+        dm.load_tokens()
+        dm.load_dicts()
 
-    gcn_model = GCN(10, 10, 10, 16, nn.ReLU())
+        #for c in text_classifier.parameters():
+        #        print(c)
 
-    class_encoder = ClassEncoder(gcn_model, word2vec_model)
+        loss_fun = torch.nn.BCELoss(reduction='sum')
+        optimizer = optim.AdamW(text_classifier.parameters(), lr=0.004)
+        g = data_manager.get_graph()
+        L = len(g.nodes())
+        features = data_manager.get_feature()
 
-    text_classifier = TextClassifier(class_encoder, (10, 768), 12)
-
-    #mock data
-
-    g = dgl.DGLGraph()
-
-    document = """
-            When our son was about 4 months old, our doctor said we could give him crafted cereal. 
-            We bought this product and put it in his bottle.
-            He loved this stuff! This cereal digests well and didn’t lock up his bowels at all. 
-            We highly recommend this cereal.
-            """
-
-    #add 12 nodes
+        train_data = dm.get_ids()
 
 
-    root = '/root/Encoder/TaxoReplica/text-classifier/processed_data/'
-    train_file = 'train-with-core-class-1000.jsonl'
-    taxonomy_file = 'taxonomy.json'
-    data_name = 'amazon'
+        for epoch in range(2): 
+                running_loss = 0.0
 
-    data_manager = TaxoDataManager(root, taxonomy_file, data_name)
+                for i, document_id in enumerate(train_data, 0):
 
-    data_manager.load_all()
-    dm = DocumentManager(train_file, root, 'amazon_train', d_encoder.Tokenize, d_encoder.Encode,  data_manager)
-    dm.load_tokens()
-    dm.load_dicts()
+                        optimizer.zero_grad()
+
+                        tokens= dm.get_tokens(document_id)
+                        target = dm.get_output_label(document_id)
+
+                        # p = (L x 1)
+                        d = torch.reshape(torch.tensor(tokens), (-1, 1))
+                        p = text_classifier(g,d, features)
+
+                        pos, nonneg = target
+                        outputs = torch.zeros(p.shape)
+
+                        for j in nonneg:
+                                outputs[j][0] = 1
+                        #        if not (i in pos):
+                        #                p[i][0] = 1
+
+                        loss = loss_fun(p, outputs)
+                        loss.backward()
+                        optimizer.step()
+
+                        # print statistics
+                        running_loss += loss.item()
+                        if i % 64 == 63:
+                                print('[%d, %5d] loss: %.3f' %
+                                        (epoch + 1, i + 1, running_loss / 2000))
+                                running_loss = 0.0
+
+
+        print('Finished Training')
+
+
+
